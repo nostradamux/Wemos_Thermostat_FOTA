a8ef92a42296ea234f7f1d679b0b6a57b741c0eb
Pablo Varela <parapavlo@hotmail.com>
2020-10-11 03:35:43 +0200

    Added new Build Target to upload via FOTA

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ const char index_html[] PROGMEM = R"rawliteral(
   <title>Temperature Threshold Output Control</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   </head><body>
-  <h2>Temperatura Consigna</h2> 
+  <h2>Temperatura Consigna Termostato</h2> 
   <h3>%TEMPERATURE% &deg;C</h3>
   <h2>ESP Arm Trigger</h2>
   <form action="/get">
